fe0b22ab5b285b9089116579234e2a9e054d103a
Moamal-2000 <moamalalaapro1@gmail.com>
2025-05-11 16:14:15 +0300

    style: edit Header stricture & add styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,27 +1,32 @@
+import Link from "next/link";
 import s from "./Header.module.scss";
 
 const Header = () => {
   return (
     <header className={s.header}>
-      <div className={s.logo}>MA</div>
-
       <nav className={s.navLinks}>
-        <ul>
-          <li>
-            <a href="#about-section">About</a>
-          </li>
-          <li>
-            <a href="#projects-section">Projects</a>
-          </li>
-          <li>
-            <a href="#contact-section">Contact</a>
-          </li>
-        </ul>
-      </nav>
+        <Link className={s.logo} href="/">
+          MA
+        </Link>
 
-      <a className={s.resume} href="#">
-        Resume
-      </a>
+        <div className={s.wrapper}>
+          <ol>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#projects">Projects</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+          </ol>
+
+          <a className={s.resume} href="#">
+            Resume
+          </a>
+        </div>
+      </nav>
     </header>
   );
 };
