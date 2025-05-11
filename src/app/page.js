8de709cee573a1469283548ca071f151092dc175
Moamal-2000 <moamalalaapro1@gmail.com>
2025-05-11 16:32:36 +0300

    init: HeroSection component

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,10 @@
+import HeroSection from "@/components/Home/HeroSection/HeroSection";
 import s from "./page.module.scss";
 
 export default function Home() {
-  return <main className={s.home}></main>;
+  return (
+    <main className={s.home}>
+      <HeroSection />
+    </main>
+  );
 }
---
+++ src/components/Home/HeroSection/HeroSection.jsx
@@ -0,0 +1,11 @@
+import s from "./HeroSection.module.scss";
+
+const HeroSection = () => {
+  return (
+    <main className={s.heroSection} id="hero">
+      HeroSection
+    </main>
+  );
+};
+
+export default HeroSection;
