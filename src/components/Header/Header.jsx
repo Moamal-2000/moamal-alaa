866ad74ccbcf6135c0dc5effc129384f12d25a51
Moamal-2000 <moamalalaapro1@gmail.com>
2025-05-11 15:33:34 +0300

    update: add HTML stricture & CSS selectors to the Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,29 @@
+import s from "./Header.module.scss";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className={s.header}>
+      <div className={s.logo}>MA</div>
+
+      <nav className={s.navLinks}>
+        <ul>
+          <li>
+            <a href="#about-section">About</a>
+          </li>
+          <li>
+            <a href="#projects-section">Projects</a>
+          </li>
+          <li>
+            <a href="#contact-section">Contact</a>
+          </li>
+        </ul>
+      </nav>
+
+      <a className={s.resume} href="#">
+        Resume
+      </a>
+    </header>
+  );
 };
 
 export default Header;
