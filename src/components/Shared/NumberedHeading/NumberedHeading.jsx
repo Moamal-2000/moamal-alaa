b06d56e2fd9d9fd820cd8784ee7a5325be766fec
Moamal-2000 <moamalalaapro1@gmail.com>
2025-05-13 16:56:08 +0300

    style: NumberedHeading component & update it to accept 'title' and 'number' props

--- src/components/Shared/NumberedHeading/NumberedHeading.jsx
+++ src/components/Shared/NumberedHeading/NumberedHeading.jsx
@@ -1,7 +1,12 @@
 import s from "./NumberedHeading.module.scss";
 
-const NumberedHeading = () => {
-  return <h2 className={s.numberedHeading}>NumberedHeading</h2>;
+const NumberedHeading = ({ title, number }) => {
+  return (
+    <h2 className={s.numberedHeading}>
+      <span>{number}.</span>
+      {title}
+    </h2>
+  );
 };
 
 export default NumberedHeading;
