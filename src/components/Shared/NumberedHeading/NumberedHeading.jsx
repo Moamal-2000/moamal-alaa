7b7891732b62be96f6675cef16f1e4084fe85247
Moamal-2000 <moamalalaapro1@gmail.com>
2025-05-13 16:27:37 +0300

    init: NumberedHeading component

---
+++ src/components/Shared/NumberedHeading/NumberedHeading.jsx
@@ -0,0 +1,7 @@
+import s from "./NumberedHeading.module.scss";
+
+const NumberedHeading = () => {
+  return <h2 className={s.numberedHeading}>NumberedHeading</h2>;
+};
+
+export default NumberedHeading;
