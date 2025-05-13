4bd8945184935cda2c5c5ed7477cfae78c2c3323
Moamal-2000 <moamalalaapro1@gmail.com>
2025-05-13 10:31:44 +0300

    add: SOCIAL_MEDIA constant data to staticData.js file

---
+++ src/data/staticData.js
@@ -0,0 +1,26 @@
+export const SOCIAL_MEDIA = [
+  {
+    media: "GitHub",
+    iconName: "github",
+    url: "https://github.com/Moamal-2000",
+    id: 1,
+  },
+  {
+    media: "LinkedIn",
+    iconName: "linkedin",
+    url: "https://www.linkedin.com/in/moamal-alaa",
+    id: 2,
+  },
+  {
+    media: "CodePen",
+    iconName: "codepen",
+    url: "https://codepen.io/moamal-2000",
+    id: 3,
+  },
+  {
+    media: "Twitter",
+    iconName: "twitter",
+    url: "https://x.com/MoamalAlaa7",
+    id: 4,
+  },
+];
