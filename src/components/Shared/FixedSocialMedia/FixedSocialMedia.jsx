4b9c5abcab0a555a97bf1fe24162d54923ef1cb4
Moamal-2000 <moamalalaapro1@gmail.com>
2025-05-13 10:34:09 +0300

    update: implement social media links in FixedSocialMedia component

--- src/components/Shared/FixedSocialMedia/FixedSocialMedia.jsx
+++ src/components/Shared/FixedSocialMedia/FixedSocialMedia.jsx
@@ -1,7 +1,22 @@
+import { SOCIAL_MEDIA } from "@/data/staticData";
 import s from "./FixedSocialMedia.module.scss";
 
 const FixedSocialMedia = () => {
-  return <div className={s.socialMedia}></div>;
+  return (
+    <div className={s.socialMedia}>
+      {SOCIAL_MEDIA.map(({ media, iconName, url, id }) => (
+        <a
+          key={id}
+          href={url}
+          target="_blank"
+          rel="noopener noreferrer"
+          title={media}
+        >
+          <SvgIcon name={iconName} />
+        </a>
+      ))}
+    </div>
+  );
 };
 
 export default FixedSocialMedia;
