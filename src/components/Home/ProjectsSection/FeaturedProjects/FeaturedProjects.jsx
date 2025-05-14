d9aec5dc9e4486a3b7408a8c3e3965e7e2631d0c
Moamal-2000 <moamalalaapro1@gmail.com>
2025-05-14 10:06:02 +0300

    init: FeaturedProjects component

---
+++ src/components/Home/ProjectsSection/FeaturedProjects/FeaturedProjects.jsx
@@ -0,0 +1,7 @@
+import s from "./FeaturedProjects.module.scss";
+
+const FeaturedProjects = () => {
+  return <div className={s.featuredProjects}></div>;
+};
+
+export default FeaturedProjects;
--- src/components/Home/ProjectsSection/ProjectsSection.jsx
+++ src/components/Home/ProjectsSection/ProjectsSection.jsx
@@ -1,10 +1,13 @@
 import NumberedHeading from "@/components/Shared/NumberedHeading/NumberedHeading";
 import s from "./ProjectsSection.module.scss";
+import FeaturedProjects from "./FeaturedProjects/FeaturedProjects";
 
 const ProjectsSection = () => {
   return (
     <section className={s.projectsSection}>
       <NumberedHeading title="Some Things I've Built" number="02" />
+
+      <FeaturedProjects />
     </section>
   );
 };
