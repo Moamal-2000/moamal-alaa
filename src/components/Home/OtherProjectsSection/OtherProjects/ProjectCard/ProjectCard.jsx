5494b644f892cfeca4eba92fa3a9c76cbc3d7e5c
Moamal-2000 <moamalalaapro1@gmail.com>
2025-05-15 12:29:21 +0300

    add: the structure of ProjectCard component

--- src/components/Home/OtherProjectsSection/OtherProjects/ProjectCard/ProjectCard.jsx
+++ src/components/Home/OtherProjectsSection/OtherProjects/ProjectCard/ProjectCard.jsx
@@ -1,9 +1,46 @@
+import SvgIcon from "@/components/Shared/SvgIcon";
 import s from "./ProjectCard.module.scss";
 
 const ProjectCard = ({ data }) => {
   const { title, description, technologies, liveUrl } = data;
 
-  return <div className={s.projectCard}></div>;
+  return (
+    <article className={s.projectCard}>
+      <header>
+        <div className={s.projectTop}>
+          <div className={s.folder}>
+            <SvgIcon name="folder" />
+          </div>
+
+          <a
+            href={liveUrl}
+            aria-label="External Link"
+            className={s.external}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <SvgIcon name="live" />
+          </a>
+        </div>
+
+        <h3 className={s.projectTitle}>
+          <a href={liveUrl} target="_blank" rel="noopener noreferrer">
+            {title}
+          </a>
+        </h3>
+
+        <p className={s.projectDescription}>{description}</p>
+      </header>
+
+      <footer>
+        <ul className={s.TechList}>
+          {technologies.map((tech) => (
+            <li key={tech}>{tech}</li>
+          ))}
+        </ul>
+      </footer>
+    </article>
+  );
 };
 
 export default ProjectCard;
