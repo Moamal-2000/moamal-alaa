58a4b041a4594e4995281b26abff63a3fd9c5dd3
Moamal-2000 <moamalalaapro1@gmail.com>
2025-05-15 12:17:46 +0300

    add: initialize OTHER_PROJECTS_DATA with 1 dummy project data

---
+++ src/data/otherProjects.js
@@ -0,0 +1,9 @@
+export const OTHER_PROJECTS_DATA = [
+  {
+    title: "Integrating Algolia Search with WordPress Multisite",
+    description:
+      "Building a custom multisite compatible WordPress plugin to build global search with Algolia",
+    liveUrl: "",
+    technologies: ["React", "Redux", "Sass"],
+  },
+];
