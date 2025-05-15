a9223f448eaf137c6cf4590e73794cfd9118460c
Moamal-2000 <moamalalaapro1@gmail.com>
2025-05-15 12:04:54 +0300

    feat: add title & archive link to OtherProjectsSection component with styles

--- src/components/Home/OtherProjectsSection/OtherProjectsSection.jsx
+++ src/components/Home/OtherProjectsSection/OtherProjectsSection.jsx
@@ -1,7 +1,13 @@
+import Link from "next/link";
 import s from "./OtherProjectsSection.module.scss";
 
 const OtherProjectsSection = () => {
-  return <section className={s.projectsSection}></section>;
+  return (
+    <section className={s.projectsSection}>
+      <h2>Other Noteworthy Projects</h2>
+      <Link href="/archive">view the archive</Link>
+    </section>
+  );
 };
 
 export default OtherProjectsSection;
