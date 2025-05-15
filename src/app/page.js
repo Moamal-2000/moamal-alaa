f62bba31ff2fa905e64009e8bf56bb4de15b3e62
Moamal-2000 <moamalalaapro1@gmail.com>
2025-05-15 18:10:15 +0300

    add: the structure of ContactSection component

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import AboutSection from "@/components/Home/AboutSection/AboutSection";
+import ContactSection from "@/components/Home/ContactSection/ContactSection";
 import HeroSection from "@/components/Home/HeroSection/HeroSection";
 import OtherProjectsSection from "@/components/Home/OtherProjectsSection/OtherProjectsSection";
 import ProjectsSection from "@/components/Home/ProjectsSection/ProjectsSection";
@@ -11,6 +12,7 @@ export default function Home() {
       <AboutSection />
       <ProjectsSection />
       <OtherProjectsSection />
+      <ContactSection />
     </main>
   );
 }
--- src/components/Home/ContactSection/ContactSection.jsx
+++ src/components/Home/ContactSection/ContactSection.jsx
@@ -1,7 +1,17 @@
 import s from "./ContactSection.module.scss";
 
 const ContactSection = () => {
-  return <section className={s.contactSection}></section>;
+  return (
+    <section className={s.contactSection}>
+      <h2>04. What's Next?</h2>
+      <h3>Get In Touch</h3>
+      <p className={s.description}>
+        I'm currently looking for new opportunities. Whether you have a question
+        or just want to say hi, I'll get back to you as soon as possible!
+      </p>
+      <a href="mailto:moamalalaapro1@gmail.com">Say Hello</a>
+    </section>
+  );
 };
 
 export default ContactSection;
