5c24e5a3e67aaeffb824446026dfe642e466d148
Moamal-2000 <moamalalaapro1@gmail.com>
2025-05-15 12:14:57 +0300

    add: folder icon to iconsData

--- src/data/iconsData.jsx
+++ src/data/iconsData.jsx
@@ -99,6 +99,21 @@ export const iconsData = [
       </svg>
     ),
   },
+  {
+    name: "folder",
+    icon: (
+      <svg
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        viewBox="0 0 24 24"
+      >
+        <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z" />
+      </svg>
+    ),
+  },
   // {
   //   name: "",
   //   icon: (
