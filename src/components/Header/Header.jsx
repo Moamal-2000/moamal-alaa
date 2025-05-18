15bf27eb2cf909d5a3bc0ab55d64cb62b9aa7eed
Moamal-2000 <moamalalaapro1@gmail.com>
2025-05-18 17:36:36 +0300

    feat: implement header visibility toggle based on scroll direction

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,51 @@
+"use client";
+
+import useScrollDirection from "@/hooks/helper/useScrollDirection";
 import Link from "next/link";
+import { useEffect, useRef, useState } from "react";
 import s from "./Header.module.scss";
 
 const Header = () => {
+  const debounceRef = useRef();
+  const [isActive, setIsActive] = useState(false);
+  const [isHidden, setIsHidden] = useState(false);
+  const scrollDirection = useScrollDirection({ initialDir: "down" });
+  const isMount = useRef(false);
+  const activeClass = isActive ? s.active : "";
+  const hiddenClass = isHidden ? s.hidden : "";
+  const headerClasses = `${s.header} ${activeClass} ${hiddenClass}`;
+
+  function handleScroll() {
+    clearTimeout(debounceRef.current);
+
+    debounceRef.current = setTimeout(() => {
+      if (scrollDirection === "down" && isMount.current) {
+        setIsHidden(true);
+        return;
+      }
+
+      setIsHidden(false);
+      setIsActive(window?.scrollY >= 50);
+    }, 50);
+  }
+
+  useEffect(() => {
+    // Invoke scroll handler on initial load
+    if (!isMount.current) {
+      handleScroll();
+      isMount.current = true;
+    }
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+      clearTimeout(debounceRef.current);
+    };
+  }, [scrollDirection]);
+
   return (
-    <header className={s.header}>
+    <header className={headerClasses}>
       <nav className={s.navLinks}>
         <Link className={s.logo} href="/">
           MA
