d04a966e592c03d708cbdd0149bf40454f9a0329
Moamal-2000 <moamalalaapro1@gmail.com>
2025-05-18 15:10:11 +0300

    init: InLineSocialMedia component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,10 @@
 import s from "./Footer.module.scss";
+import InLineSocialMedia from "./InLineSocialMedia/InLineSocialMedia";
 
 const Footer = () => {
   return (
     <footer className={s.footer}>
+      <InLineSocialMedia />
       <p className={s.copyRights}>
         Designed by Brittany Chiang and Built by Moamal Alaa with Next.js
       </p>
---
+++ src/components/Footer/InLineSocialMedia/InLineSocialMedia.jsx
@@ -0,0 +1,7 @@
+import s from "./InLineSocialMedia.module.scss";
+
+const InLineSocialMedia = () => {
+  return <ul className={s.socialMedia}></ul>;
+};
+
+export default InLineSocialMedia;
