0a611fea44514b4cc3167a0a3474ea2333a3c8b4
Moamal-2000 <moamalalaapro1@gmail.com>
2025-05-18 15:08:12 +0300

    style: add copyright text with its styles in the footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,13 @@
 import s from "./Footer.module.scss";
 
 const Footer = () => {
-  return <footer className={s.footer}></footer>;
+  return (
+    <footer className={s.footer}>
+      <p className={s.copyRights}>
+        Designed by Brittany Chiang and Built by Moamal Alaa with Next.js
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
