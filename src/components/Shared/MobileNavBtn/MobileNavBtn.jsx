b1cbdaf983bdb919ac8509d6593c13623e057cf1
Moamal-2000 <moamalalaapro1@gmail.com>
2025-05-22 02:45:09 +0300

    feat: toggle isMobileNavOpen global state on click for MobileNavBtn

--- src/components/Shared/MobileNavBtn/MobileNavBtn.jsx
+++ src/components/Shared/MobileNavBtn/MobileNavBtn.jsx
@@ -1,8 +1,23 @@
+"use client";
+
+import useGlobalStore from "@/stores/global/useGlobalStore";
 import s from "./MobileNavBtn.module.scss";
 
 const MobileNavBtn = () => {
+  const { isMobileNavOpen, toggleMobileNav } = useGlobalStore();
+  const activeClass = isMobileNavOpen ? s.active : "";
+
+  function handleClick() {
+    toggleMobileNav();
+  }
+
   return (
-    <button type="button" className={s.mobileNavBtn} aria-label="Menu Button">
+    <button
+      type="button"
+      className={`${s.mobileNavBtn} ${activeClass}`}
+      aria-label="Menu Button"
+      onClick={handleClick}
+    >
       <div className={s.icon} />
     </button>
   );
--- src/stores/global/useGlobalStore.js
+++ src/stores/global/useGlobalStore.js
@@ -1,3 +1,9 @@
 import { create } from "zustand";
 
-export const useGlobalStore = create((set) => ({}));
+export const useGlobalStore = create((set) => ({
+  isMobileNavOpen: false,
+  toggleMobileNav: (value) =>
+    set((state) => ({ isMobileNavOpen: value || !state.isMobileNavOpen })),
+}));
+
+export default useGlobalStore;
