f8609ed61e27ae515efec55a163e91d81e484ac6
Moamal-2000 <moamalalaapro1@gmail.com>
2025-05-22 22:29:41 +0300

    update: add jsx structure & style not-found page

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,7 +1,16 @@
+import Link from "next/link";
 import s from "./NotFoundPage.module.scss";
 
 const NotFoundPage = () => {
-  return <main className={s.notFoundPage}></main>;
+  return (
+    <main className={s.notFoundPage}>
+      <h1>404</h1>
+      <h2>Page Not Found</h2>
+      <Link href="/" className={s.homeButton}>
+        Go Home
+      </Link>
+    </main>
+  );
 };
 
 export default NotFoundPage;
