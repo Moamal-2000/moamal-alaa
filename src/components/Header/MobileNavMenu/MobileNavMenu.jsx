4bb59032b453386af295f5f0ca2bcd2bf97da3b9
Moamal-2000 <moamalalaapro1@gmail.com>
2025-05-22 04:35:58 +0300

    update: add jsx structure & styles to MobileNavMenu component

--- src/components/Header/MobileNavMenu/MobileNavMenu.jsx
+++ src/components/Header/MobileNavMenu/MobileNavMenu.jsx
@@ -1,7 +1,25 @@
+import { NAV_LINKS } from "@/data/staticData";
+import Link from "next/link";
 import s from "./MobileNavMenu.module.scss";
 
 const MobileNavMenu = () => {
-  return <nav className={s.mobileNavMenu}></nav>;
+  return (
+    <aside className={s.mobileMenu}>
+      <nav>
+        <ol>
+          {NAV_LINKS.map(({ title, id }) => (
+            <li key={id}>
+              <a href={`#${title.toLowerCase()}`}>{title}</a>
+            </li>
+          ))}
+        </ol>
+
+        <Link className={s.resume} href="/resume.pdf">
+          Resume
+        </Link>
+      </nav>
+    </aside>
+  );
 };
 
 export default MobileNavMenu;
