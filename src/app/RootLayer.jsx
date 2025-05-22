6bcdf7e0232d4d3a87f882d3b7479bbf68215c5c
Moamal-2000 <moamalalaapro1@gmail.com>
2025-05-22 05:38:57 +0300

    feat: add click handler to close mobile nav when RootLayer is clicked

--- src/app/RootLayer.jsx
+++ src/app/RootLayer.jsx
@@ -4,10 +4,20 @@ import useGlobalStore from "@/stores/global/useGlobalStore";
 import s from "./RootLayer.module.scss";
 
 const RootLayer = ({ children }) => {
-  const { isMobileNavOpen } = useGlobalStore();
+  const { isMobileNavOpen, updateGlobalState } = useGlobalStore();
   const blurClass = isMobileNavOpen ? s.blur : "";
 
-  return <div className={`${s.layer} ${blurClass}`}>{children}</div>;
+  function handleClick() {
+    if (!isMobileNavOpen) return;
+
+    updateGlobalState("isMobileNavOpen", false);
+  }
+
+  return (
+    <div className={`${s.layer} ${blurClass}`} onClick={handleClick}>
+      {children}
+    </div>
+  );
 };
 
 export default RootLayer;
