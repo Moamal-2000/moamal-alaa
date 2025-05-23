daa570a82225ffb05959eda55bf3b87bd43f85ed
Moamal-2000 <moamalalaapro1@gmail.com>
2025-05-24 01:11:17 +0300

    update: footer to use AnchorLink components for credits

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,3 +1,4 @@
+import AnchorLink from "../Shared/AnchorLink/AnchorLink";
 import s from "./Footer.module.scss";
 import InLineSocialMedia from "./InLineSocialMedia/InLineSocialMedia";
 
@@ -6,7 +7,30 @@ const Footer = () => {
     <footer className={s.footer}>
       <InLineSocialMedia />
       <p className={s.copyRights}>
-        Designed by Brittany Chiang and Built by Moamal Alaa with Next.js
+        Designed by{" "}
+        <AnchorLink
+          href="https://www.linkedin.com/in/bchiang7"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Brittany Chiang
+        </AnchorLink>{" "}
+        and Built by{" "}
+        <AnchorLink
+          href="https://www.linkedin.com/in/moamal-alaa/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Moamal Alaa
+        </AnchorLink>{" "}
+        with{" "}
+        <AnchorLink
+          href="https://nextjs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Next.js
+        </AnchorLink>
       </p>
     </footer>
   );
