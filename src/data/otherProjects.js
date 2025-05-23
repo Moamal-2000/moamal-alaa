e267b4d958df237a15b2a974e083214c57bee4dd
Moamal-2000 <moamalalaapro1@gmail.com>
2025-05-23 21:32:44 +0300

    update: reorder OTHER_PROJECTS_DATA

--- src/data/otherProjects.js
+++ src/data/otherProjects.js
@@ -1,25 +1,5 @@
 export const OTHER_PROJECTS_DATA = [
   {
-    title: "Call of Duty 2 – Jumpers Heaven Statistics Website",
-    description:
-      "A dynamic website that displays real-time statistics for Call of Duty 2 players on Jumpers Heaven servers. Still in active development, the platform aims to offer detailed insights and leaderboard data.",
-    year: "2025",
-    liveUrl: "https://jh-leaderboard.netlify.app",
-    repoUrl: "https://github.com/Moamal-2000/JHLeaderboard",
-    technologies: ["Next.js", "MessagePack", "Sass", "Redux Toolkit"],
-    id: 1,
-  },
-  {
-    title: "Custom React Hooks Library",
-    description:
-      "A collection of reusable and well-documented custom React hooks for boosting productivity and consistency across React projects. Includes live previews and code highlighting",
-    year: "2024",
-    liveUrl: "https://hooks-time.netlify.app",
-    repoUrl: "https://github.com/Moamal-2000/custom-hooks",
-    technologies: ["React", "PWA", "JSZip", "react-syntax-highlighter"],
-    id: 2,
-  },
-  {
     title: "Challenges with JavaScript",
     description:
       "A set of JavaScript challenges focused on solving real-world interactivity problems using only HTML, CSS, and JavaScript. Built to sharpen DOM manipulation, event handling, and logic-building skills.",
@@ -27,7 +7,7 @@ export const OTHER_PROJECTS_DATA = [
     liveUrl: "https://moamal-2000.github.io/simple-javascript-projects/",
     repoUrl: "https://github.com/Moamal-2000/simple-javascript-projects",
     technologies: ["HTML", "CSS", "Sass", "JavaScript"],
-    id: 3,
+    id: 1,
   },
   {
     title: "Responsive Landing Page",
@@ -37,7 +17,7 @@ export const OTHER_PROJECTS_DATA = [
     liveUrl: "https://moamal-2000.github.io/template2",
     repoUrl: "https://github.com/Moamal-2000/template2",
     technologies: ["HTML", "CSS", "Sass", "JavaScript"],
-    id: 4,
+    id: 2,
   },
   {
     title: "Static Country Info App",
@@ -47,7 +27,7 @@ export const OTHER_PROJECTS_DATA = [
     liveUrl: "https://moamal-2000.github.io/rest-api-countries",
     repoUrl: "https://github.com/Moamal-2000/rest-api-countries",
     technologies: ["HTML", "CSS", "JavaScript"],
-    id: 5,
+    id: 3,
   },
   {
     title: "Superado Landing Page",
@@ -57,6 +37,26 @@ export const OTHER_PROJECTS_DATA = [
     liveUrl: "https://superado-landing.netlify.app",
     repoUrl: "https://github.com/Moamal-2000/superado-landing",
     technologies: ["React", "Vite", "Sass"],
+    id: 4,
+  },
+  {
+    title: "Custom React Hooks Library",
+    description:
+      "A collection of reusable and well-documented custom React hooks for boosting productivity and consistency across React projects. Includes live previews and code highlighting",
+    year: "2024",
+    liveUrl: "https://hooks-time.netlify.app",
+    repoUrl: "https://github.com/Moamal-2000/custom-hooks",
+    technologies: ["React", "PWA", "JSZip", "syntax highlighter"],
+    id: 5,
+  },
+  {
+    title: "Call of Duty 2 – Jumpers Heaven Statistics Website",
+    description:
+      "A dynamic website that displays real-time statistics for Call of Duty 2 players on Jumpers Heaven servers. Still in active development, the platform aims to offer detailed insights and leaderboard data.",
+    year: "2025",
+    liveUrl: "https://jh-leaderboard.netlify.app",
+    repoUrl: "https://github.com/Moamal-2000/JHLeaderboard",
+    technologies: ["Next.js", "MessagePack", "Sass", "Redux Toolkit"],
     id: 6,
   },
-];
+].reverse();
