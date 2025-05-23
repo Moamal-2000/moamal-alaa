214e77b1942cc9265b8df42ebb91de11e4fa6b4d
Moamal-2000 <moamalalaapro1@gmail.com>
2025-05-23 20:42:34 +0300

    update: move jsx/styles from ProjectsTable to ProjectsTBody component

--- src/components/Archive/ProjectsTable/ProjectsTBody/ProjectsTBody.jsx
+++ src/components/Archive/ProjectsTable/ProjectsTBody/ProjectsTBody.jsx
@@ -1,9 +1,43 @@
-import s from './ProjectsTBody.module.scss'
+import SvgIcon from "@/components/Shared/SvgIcon";
+import { OTHER_PROJECTS_DATA } from "@/data/otherProjects";
+import s from "./ProjectsTBody.module.scss";
 
 const ProjectsTBody = () => {
   return (
-    <div></div>
-  )
-}
+    <tbody className={s.tbody}>
+      {OTHER_PROJECTS_DATA.map(
+        ({ id, year, title, technologies, liveUrl, repoUrl }) => (
+          <tr key={id}>
+            <td className={s.year}>{year}</td>
+            <td className={s.title}>{title}</td>
+            <td className={`${s.tech} ${s.hideOnMobile}`}>
+              {technologies?.map((tech, index) => (
+                <span key={index}>
+                  {tech}
+                  {index < technologies.length - 1 && (
+                    <span className={s.separator}>·</span>
+                  )}
+                </span>
+              ))}
+            </td>
+            <td className={s.links}>
+              {liveUrl && (
+                <a href={liveUrl} target="_blank" rel="noopener noreferrer">
+                  <SvgIcon name="live" />
+                </a>
+              )}
 
-export default ProjectsTBody
+              {repoUrl && (
+                <a href={liveUrl} target="_blank" rel="noopener noreferrer">
+                  <SvgIcon name="github" />
+                </a>
+              )}
+            </td>
+          </tr>
+        )
+      )}
+    </tbody>
+  );
+};
+
+export default ProjectsTBody;
--- src/components/Archive/ProjectsTable/ProjectsTable.jsx
+++ src/components/Archive/ProjectsTable/ProjectsTable.jsx
@@ -1,5 +1,3 @@
-import SvgIcon from "@/components/Shared/SvgIcon";
-import { OTHER_PROJECTS_DATA } from "@/data/otherProjects";
 import s from "./ProjectsTable.module.scss";
 import ProjectsTBody from "./ProjectsTBody/ProjectsTBody";
 
@@ -16,39 +14,6 @@ const ProjectsTable = () => {
           </tr>
         </thead>
 
-        <tbody>
-          {OTHER_PROJECTS_DATA.map(
-            ({ id, year, title, technologies, liveUrl, repoUrl }) => (
-              <tr key={id}>
-                <td className={s.year}>{year}</td>
-                <td className={s.title}>{title}</td>
-                <td className={`${s.tech} ${s.hideOnMobile}`}>
-                  {technologies?.map((tech, index) => (
-                    <span key={index}>
-                      {tech}
-                      {index < technologies.length - 1 && (
-                        <span className={s.separator}>·</span>
-                      )}
-                    </span>
-                  ))}
-                </td>
-                <td className={s.links}>
-                  {liveUrl && (
-                    <a href={liveUrl} target="_blank" rel="noopener noreferrer">
-                      <SvgIcon name="live" />
-                    </a>
-                  )}
-
-                  {repoUrl && (
-                    <a href={liveUrl} target="_blank" rel="noopener noreferrer">
-                      <SvgIcon name="github" />
-                    </a>
-                  )}
-                </td>
-              </tr>
-            )
-          )}
-        </tbody>
         <ProjectsTBody />
       </table>
     </div>
