83bcc094c8e63627689246d741d6031d3d4266cb
Moamal-2000 <moamalalaapro1@gmail.com>
2025-05-23 21:04:15 +0300

    update: add 'Custom Hooks Library' to worthy projects section

--- src/data/otherProjects.js
+++ src/data/otherProjects.js
@@ -10,13 +10,14 @@ export const OTHER_PROJECTS_DATA = [
     id: 1,
   },
   {
-    title: "Some title",
-    description: "Description...",
-    year: "2023",
-    liveUrl: "",
-    repoUrl: "",
-    technologies: ["Tech", "Tech", "Tech"],
     id: 2,
+    title: "Custom React Hooks Library",
+    description:
+      "A collection of reusable and well-documented custom React hooks for boosting productivity and consistency across React projects. Includes live previews and code highlighting",
+    year: "2024",
+    liveUrl: "https://hooks-time.netlify.app",
+    repoUrl: "https://github.com/Moamal-2000/custom-hooks",
+    technologies: ["React", "PWA", "JSZip", "react-syntax-highlighter"],
   },
   {
     title: "Some title",
