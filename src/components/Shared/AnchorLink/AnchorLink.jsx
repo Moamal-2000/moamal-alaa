71c17928ccb9c10fd68a2f3afd3b9711a9ef9353
Moamal-2000 <moamalalaapro1@gmail.com>
2025-05-24 00:56:39 +0300

    rename: Link component with AnchorLink

---
+++ src/components/Shared/AnchorLink/AnchorLink.jsx
@@ -0,0 +1,7 @@
+import s from "./AnchorLink.module.scss";
+
+const AnchorLink = () => {
+  return <a href="" className={s.link}></a>;
+};
+
+export default AnchorLink;
--- src/components/Shared/Link/Link.jsx
+++
@@ -1,7 +0,0 @@
-import s from "./Link.module.scss";
-
-const Link = () => {
-  return <a href="" className={s.link}></a>;
-};
-
-export default Link;
