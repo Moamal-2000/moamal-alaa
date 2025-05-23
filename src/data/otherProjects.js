b5a8ca78ed51b70db698eb6b5543131c9e964a08
Moamal-2000 <moamalalaapro1@gmail.com>
2025-05-23 20:58:22 +0300

    update: add 'Call of Duty 2 Statistics Website' to worthy projects section

--- src/data/otherProjects.js
+++ src/data/otherProjects.js
@@ -1,11 +1,12 @@
 export const OTHER_PROJECTS_DATA = [
   {
-    title: "Some title",
-    description: "Description...",
-    year: "2023",
-    liveUrl: "",
-    repoUrl: "",
-    technologies: ["Tech", "Tech", "Tech"],
+    title: "Call of Duty 2 – Jumpers Heaven Statistics Website",
+    description:
+      "A dynamic website that displays real-time statistics for Call of Duty 2 players on Jumpers Heaven servers. Still in active development, the platform aims to offer detailed insights and leaderboard data.",
+    year: "2025",
+    liveUrl: "https://jh-leaderboard.netlify.app",
+    repoUrl: "https://github.com/Moamal-2000/JHLeaderboard",
+    technologies: ["Next.js", "MessagePack", "Sass", "Redux Toolkit"],
     id: 1,
   },
   {
