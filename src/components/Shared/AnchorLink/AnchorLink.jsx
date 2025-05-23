748c8b0ec514c9a379498a874474d36aa71faed5
Moamal-2000 <moamalalaapro1@gmail.com>
2025-05-24 01:07:50 +0300

    feat: build AnchorLink component with styling

--- src/components/Shared/AnchorLink/AnchorLink.jsx
+++ src/components/Shared/AnchorLink/AnchorLink.jsx
@@ -1,7 +1,11 @@
 import s from "./AnchorLink.module.scss";
 
-const AnchorLink = () => {
-  return <a href="" className={s.link}></a>;
+const AnchorLink = ({ children, ...props }) => {
+  return (
+    <a className={s.link} {...props}>
+      {children}
+    </a>
+  );
 };
 
 export default AnchorLink;
