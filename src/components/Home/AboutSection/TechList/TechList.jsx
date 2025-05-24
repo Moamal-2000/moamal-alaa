87703d9855d48e83607c074da5abdeb566d48bea
Moamal-2000 <moamalalaapro1@gmail.com>
2025-05-24 04:38:13 +0300

    feat: display box info about each technology in About section

--- src/components/Home/AboutSection/TechList/TechList.jsx
+++ src/components/Home/AboutSection/TechList/TechList.jsx
@@ -4,8 +4,15 @@ import s from "./TechList.module.scss";
 const TechList = () => {
   return (
     <ul className={s.techList}>
-      {TECHNOLOGIES.map(({ name }) => (
-        <li key={name}>{name}</li>
+      {TECHNOLOGIES.map(({ name, description, id }) => (
+        <li key={`${id}-${name}`}>
+          {name}
+
+          <div className={s.infoBox}>
+            <b>{name}</b>
+            <p>{description}</p>
+          </div>
+        </li>
       ))}
     </ul>
   );
