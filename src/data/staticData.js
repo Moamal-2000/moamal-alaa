ea5e64aa9042872f77e165cb80e86a970a68281d
Moamal-2000 <moamalalaapro1@gmail.com>
2025-05-24 04:20:39 +0300

    add: TECHNOLOGIES data structure with descriptions for various technologies

--- src/data/staticData.js
+++ src/data/staticData.js
@@ -71,3 +71,36 @@ export const FEATURED_PROJECTS = [
     id: 2,
   },
 ];
+
+export const TECHNOLOGIES = [
+  {
+    name: "JavaScript (ES6+)",
+    description: `A versatile scripting language for web development, enabling dynamic and interactive websites. ES6+ adds modern features like arrow functions, classes, and modules.`,
+    id: 1,
+  },
+  {
+    name: "Sass",
+    description: `A preprocessor for CSS, combining CSS and SCSS features for enhanced styling and organization.`,
+    id: 2,
+  },
+  {
+    name: "React",
+    description: `A JavaScript library for building user interfaces using reusable components and a virtual DOM for efficient updates.`,
+    id: 3,
+  },
+  {
+    name: "Next",
+    description: `A React framework for server-side rendering, static site generation, and building fast, SEO-friendly web apps.`,
+    id: 4,
+  },
+  {
+    name: "Progressive Web App",
+    description: `A web application that uses modern web capabilities to deliver an app-like experience, including offline support and installability.`,
+    id: 5,
+  },
+  {
+    name: "Redux/Toolkit",
+    description: `A state management library, simplifying complex state management and ensuring consistency across the application.`,
+    id: 6,
+  },
+];
