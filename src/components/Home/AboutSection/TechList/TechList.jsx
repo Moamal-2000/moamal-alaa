754a2bd27e5d966d4b6d220915c4534a94c984ce
Moamal-2000 <moamalalaapro1@gmail.com>
2025-05-24 04:23:34 +0300

    refactor: replace hardcoded technology list with dynamic mapping from TECHNOLOGIES data structure

--- src/components/Home/AboutSection/TechList/TechList.jsx
+++ src/components/Home/AboutSection/TechList/TechList.jsx
@@ -1,14 +1,12 @@
+import { TECHNOLOGIES } from "@/data/staticData";
 import s from "./TechList.module.scss";
 
 const TechList = () => {
   return (
     <ul className={s.techList}>
-      <li>JavaScript (ES6+)</li>
-      <li>Sass</li>
-      <li>React</li>
-      <li>Next</li>
-      <li>Progressive Web App</li>
-      <li>Redux/Toolkit</li>
+      {TECHNOLOGIES.map(({ name }) => (
+        <li key={name}>{name}</li>
+      ))}
     </ul>
   );
 };
