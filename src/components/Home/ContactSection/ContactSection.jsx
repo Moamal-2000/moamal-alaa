0a8f59ffcadcdd8275790da7458b9cda69d829e1
Moamal-2000 <moamalalaapro1@gmail.com>
2025-05-27 06:25:44 +0300

    feat(ContactSection): implement fade-in animation on view for contact section

--- src/components/Home/ContactSection/ContactSection.jsx
+++ src/components/Home/ContactSection/ContactSection.jsx
@@ -1,8 +1,18 @@
+"use client";
+
+import { fadeInOnViewMotionProps } from "@/functions/motionConfig";
+import { motion } from "framer-motion";
 import s from "./ContactSection.module.scss";
 
 const ContactSection = () => {
   return (
-    <section className={s.contactSection} id="contact">
+    <motion.section
+      className={s.contactSection}
+      id="contact"
+      {...fadeInOnViewMotionProps({
+        visibilityThreshold: 0.8,
+      })}
+    >
       <h2>04. What's Next?</h2>
       <h3>Get In Touch</h3>
       <p className={s.description}>
@@ -10,7 +20,7 @@ const ContactSection = () => {
         or just want to say hi, I'll get back to you as soon as possible!
       </p>
       <a href="mailto:moamalalaapro1@gmail.com">Say Hello</a>
-    </section>
+    </motion.section>
   );
 };
 
