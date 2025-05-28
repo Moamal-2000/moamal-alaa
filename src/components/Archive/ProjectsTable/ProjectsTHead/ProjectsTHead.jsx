fee94f62f942f3b3235907db0b78c7c26d10b6e6
Moamal-2000 <moamalalaapro1@gmail.com>
2025-05-28 06:09:39 +0300

    refactor: add jsx/styles to ProjectsTHead component

--- src/components/Archive/ProjectsTable/ProjectsTHead/ProjectsTHead.jsx
+++ src/components/Archive/ProjectsTable/ProjectsTHead/ProjectsTHead.jsx
@@ -1,9 +1,20 @@
-import s from './ProjectsTHead.module.scss'
+"use client";
+
+import { fadeInOnViewMotionProps } from "@/functions/motionConfig";
+import { motion } from "motion/react";
+import s from "./ProjectsTHead.module.scss";
 
 const ProjectsTHead = () => {
   return (
-    <div>ProjectsTHead</div>
-  )
-}
+    <motion.thead {...fadeInOnViewMotionProps({ duration: 0.4 })}>
+      <tr>
+        <th>Year</th>
+        <th>Title</th>
+        <th className={s.hideOnMobile}>Built with</th>
+        <th>Link</th>
+      </tr>
+    </motion.thead>
+  );
+};
 
-export default ProjectsTHead
+export default ProjectsTHead;
--- src/components/Archive/ProjectsTable/ProjectsTable.jsx
+++ src/components/Archive/ProjectsTable/ProjectsTable.jsx
@@ -1,7 +1,3 @@
-"use client";
-
-import { fadeInOnViewMotionProps } from "@/functions/motionConfig";
-import { motion } from "framer-motion";
 import s from "./ProjectsTable.module.scss";
 import ProjectsTBody from "./ProjectsTBody/ProjectsTBody";
 import ProjectsTHead from "./ProjectsTHead/ProjectsTHead";
@@ -10,19 +6,6 @@ const ProjectsTable = () => {
   return (
     <div className={s.tableWrapper}>
       <table className={s.table}>
-        <motion.thead
-          {...fadeInOnViewMotionProps({
-            duration: 0.4,
-          })}
-        >
-          <tr>
-            <th>Year</th>
-            <th>Title</th>
-            <th className={s.hideOnMobile}>Built with</th>
-            <th>Link</th>
-          </tr>
-        </motion.thead>
-
         <ProjectsTHead />
         <ProjectsTBody />
       </table>
