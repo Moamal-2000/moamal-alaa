cb96e7487f10fe0a608a7b3f6a7c4d98718258b4
Moamal-2000 <moamalalaapro1@gmail.com>
2025-05-31 09:46:48 +0300

    feat(TechList): show box info on-hover differently beside tech list

--- src/components/Home/AboutSection/TechList/TechList.jsx
+++ src/components/Home/AboutSection/TechList/TechList.jsx
@@ -1,20 +1,40 @@
+"use client";
+
 import { TECHNOLOGIES } from "@/data/staticData";
+import { AnimatePresence, motion } from "motion/react";
+import { useState } from "react";
 import s from "./TechList.module.scss";
 
 const TechList = () => {
-  return (
-    <ul className={s.techList}>
-      {TECHNOLOGIES.map(({ name, description, id }) => (
-        <li key={`${id}-${name}`}>
-          {name}
+  const [activeIndex, setActiveIndex] = useState(0);
 
-          <div className={s.infoBox}>
-            <b>{name}</b>
-            <p>{description}</p>
-          </div>
-        </li>
-      ))}
-    </ul>
+  return (
+    <div className={s.wrapper}>
+      <ul className={s.techList}>
+        {TECHNOLOGIES.map(({ name, id }, index) => (
+          <li
+            key={`${id}-${name}`}
+            className={index === activeIndex ? s.active : ""}
+            onMouseEnter={() => setActiveIndex(index)}
+          >
+            {name}
+          </li>
+        ))}
+      </ul>
+
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={activeIndex}
+          className={s.infoBox}
+          initial={{ opacity: 0, y: 5 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -5 }}
+          transition={{ duration: 0.2, ease: "easeInOut" }}
+        >
+          <p>{TECHNOLOGIES[activeIndex].description}</p>
+        </motion.div>
+      </AnimatePresence>
+    </div>
   );
 };
 
