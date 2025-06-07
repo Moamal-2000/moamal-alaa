73f97d357b09da31e805a3d6098360c4324816a1
Moamal-2000 <moamalalaapro1@gmail.com>
2025-06-07 17:17:26 +0300

    init: InstallPWAButton component

---
+++ src/components/Header/MobileNavMenu/InstallPWAButton/InstallPWAButton.jsx
@@ -0,0 +1,11 @@
+import s from "./InstallPWAButton.module.scss";
+
+const InstallPWAButton = () => {
+  return (
+    <button type="button" className={s.pwaButton}>
+      InstallPWAButton
+    </button>
+  );
+};
+
+export default InstallPWAButton;
