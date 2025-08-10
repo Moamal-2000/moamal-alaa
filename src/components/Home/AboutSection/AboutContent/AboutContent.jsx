79624af14217e381310d880fd6d0b30779dbc058
Moamal-2000 <moamalalaapro1@gmail.com>
2025-08-10 17:23:37 +0300

    refactor(AboutSection): move text content to AboutContent component for cleaner JSX

--- src/components/Home/AboutSection/AboutContent/AboutContent.jsx
+++ src/components/Home/AboutSection/AboutContent/AboutContent.jsx
@@ -1,9 +1,26 @@
-import s from './AboutContent.module.scss'
+import s from "./AboutContent.module.scss";
 
 const AboutContent = () => {
   return (
-    <div>AboutContent</div>
-  )
-}
+    <div className={s.content}>
+      <p className={s.firstParagraph}>
+        Hi, I’m Moamal, I enjoy building things for the web. I believe websites
+        should feel smooth, look great, and work for everyone. That’s why I
+        focus on creating interfaces that are not only functional but also
+        enjoyable to interact with.
+      </p>
 
-export default AboutContent
+      <p>
+        I’ve been exploring front-end development for over four years now. It
+        all started with writing HTML and CSS, which felt like drawing with
+        code, I loved bringing designs to life on the screen. That curiosity led
+        me to JavaScript and, eventually, to the world of modern front-end
+        technologies I work with today.
+      </p>
+
+      <p>Here are a few technologies I’ve been working with recently:</p>
+    </div>
+  );
+};
+
+export default AboutContent;
--- src/components/Home/AboutSection/AboutSection.jsx
+++ src/components/Home/AboutSection/AboutSection.jsx
@@ -3,6 +3,7 @@
 import NumberedHeading from "@/components/Shared/NumberedHeading/NumberedHeading";
 import { fadeInOnViewMotionProps } from "@/functions/motionConfig";
 import { motion } from "motion/react";
+import AboutContent from "./AboutContent/AboutContent";
 import s from "./AboutSection.module.scss";
 import TechList from "./TechList/TechList";
 
@@ -17,26 +18,7 @@ const AboutSection = () => {
       })}
     >
       <NumberedHeading title="About Me" number="01" />
-
-      <div className={s.content}>
-        <p className={s.firstParagraph}>
-          Hi, I’m Moamal, I enjoy building things for the web. I believe
-          websites should feel smooth, look great, and work for everyone. That’s
-          why I focus on creating interfaces that are not only functional but
-          also enjoyable to interact with.
-        </p>
-
-        <p>
-          I’ve been exploring front-end development for over four years now. It
-          all started with writing HTML and CSS, which felt like drawing with
-          code, I loved bringing designs to life on the screen. That curiosity
-          led me to JavaScript and, eventually, to the world of modern front-end
-          technologies I work with today.
-        </p>
-
-        <p>Here are a few technologies I’ve been working with recently:</p>
-      </div>
-
+      <AboutContent />
       <TechList />
     </motion.section>
   );
