d98c3b41642add06efa860fe06546b54e68a7b92
Moamal-2000 <moamalalaapro1@gmail.com>
2025-09-02 07:31:03 +0300

    init: OtherProjectsHeader component

---
+++ src/components/Home/OtherProjectsSection/OtherProjectsHeader/OtherProjectsHeader.jsx
@@ -0,0 +1,9 @@
+import s from './OtherProjectsHeader.module.scss'
+
+const OtherProjectsHeader = () => {
+  return (
+    <div>OtherProjectsHeader</div>
+  )
+}
+
+export default OtherProjectsHeader
