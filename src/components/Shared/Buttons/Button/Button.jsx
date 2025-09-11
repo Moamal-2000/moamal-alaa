2502da2a85731fa6ca1042d537203e8f236016a6
Moamal-2000 <moamalalaapro1@gmail.com>
2025-09-11 20:50:16 +0300

    rename(component): rename Button to LinkButton for clarity

--- src/components/Shared/Buttons/Button/Button.jsx
+++
@@ -1,9 +0,0 @@
-import s from './Button.module.scss'
-
-const Button = () => {
-  return (
-    <div>Button</div>
-  )
-}
-
-export default Button
---
+++ src/components/Shared/Buttons/LinkButton/LinkButton.jsx
@@ -0,0 +1,9 @@
+import s from './LinkButton.module.scss'
+
+const LinkButton = () => {
+  return (
+    <div>LinkButton</div>
+  )
+}
+
+export default LinkButton
