908c5ae53327a9f2c862dfb3e182ba56a2ed301a
Moamal-2000 <moamalalaapro1@gmail.com>
2025-10-15 07:25:11 +0300

    update(ProjectLinks): format component using prettier formatter

--- src/components/Home/OtherProjectsSection/OtherProjects/ProjectCard/ProjectHeader/ProjectLinks/ProjectLinks.jsx
+++ src/components/Home/OtherProjectsSection/OtherProjects/ProjectCard/ProjectHeader/ProjectLinks/ProjectLinks.jsx
@@ -1,30 +1,32 @@
-import SvgIcon from '@/components/Shared/SvgIcon'
-import s from './ProjectLinks.module.scss'
+import SvgIcon from "@/components/Shared/SvgIcon";
+import s from "./ProjectLinks.module.scss";
 
 const ProjectLinks = ({ repoUrl, liveUrl }) => {
-  return <div className={s.projectLinks}>
-          {repoUrl && (
-            <a
-              href={repoUrl}
-              title="GitHub Link"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <SvgIcon name="github" />
-            </a>
-          )}
+  return (
+    <div className={s.projectLinks}>
+      {repoUrl && (
+        <a
+          href={repoUrl}
+          title="GitHub Link"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <SvgIcon name="github" />
+        </a>
+      )}
 
-          {liveUrl && (
-            <a
-              href={liveUrl}
-              title="External Link"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <SvgIcon name="live" />
-            </a>
-          )}
-        </div>
-}
+      {liveUrl && (
+        <a
+          href={liveUrl}
+          title="External Link"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <SvgIcon name="live" />
+        </a>
+      )}
+    </div>
+  );
+};
 
-export default ProjectLinks
+export default ProjectLinks;
