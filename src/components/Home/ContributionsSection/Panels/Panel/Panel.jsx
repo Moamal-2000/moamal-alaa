bdfbfbff34085aa4741dc4de82879b0054869e0d
Moamal-2000 <moamalalaapro1@gmail.com>
2025-11-22 11:00:14 +0300

    refactor(Panels): extract panel structure into Panel component

--- src/components/Home/ContributionsSection/Panels/Panel/Panel.jsx
+++ src/components/Home/ContributionsSection/Panels/Panel/Panel.jsx
@@ -1,9 +1,42 @@
-import s from './Panel.module.scss'
+import { getRepoFullName } from "@/functions/contributions";
+import Link from "next/link";
+import s from "./Panel.module.scss";
 
-const Panel = () => {
+const Panel = ({ contribution, index, activeTabId }) => {
   return (
-    <div>Panel</div>
-  )
-}
+    <div
+      key={getRepoFullName(contribution)}
+      role="tabpanel"
+      className={`${s.panel} ${activeTabId === index ? s.show : s.hidden}`}
+    >
+      <Link
+        href={contribution.repository.url}
+        target="_blank"
+        rel="noopener noreferrer"
+        className={s.title}
+        tabIndex={activeTabId === index ? 0 : -1}
+      >
+        {getRepoFullName(contribution).toLocaleLowerCase()}
+      </Link>
 
-export default Panel
+      <p className={s.description}>{contribution.repository.description}</p>
+
+      <ul className={s.pullRequests}>
+        {contribution.prs.map((pr) => (
+          <li key={pr.url}>
+            <Link
+              href={pr.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              tabIndex={activeTabId === index ? 0 : -1}
+            >
+              {pr.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Panel;
--- src/components/Home/ContributionsSection/Panels/Panels.jsx
+++ src/components/Home/ContributionsSection/Panels/Panels.jsx
@@ -1,44 +1,16 @@
-import { getRepoFullName } from "@/functions/contributions";
-import Link from "next/link";
+import Panel from "./Panel/Panel";
 import s from "./Panels.module.scss";
 
 const Panels = ({ contributionsToDisplay, activeTabId }) => {
   return (
     <div className={s.panels}>
       {contributionsToDisplay.map((contribution, index) => (
-        <div
-          key={getRepoFullName(contribution)}
-          id={`panel-${index}`}
-          role="tabpanel"
-          className={`${s.panel} ${activeTabId === index ? s.show : s.hidden}`}
-        >
-          <Link
-            href={contribution.repository.url}
-            target="_blank"
-            rel="noopener noreferrer"
-            className={s.title}
-            tabIndex={activeTabId === index ? 0 : -1}
-          >
-            {getRepoFullName(contribution).toLocaleLowerCase()}
-          </Link>
-
-          <p className={s.description}>{contribution.repository.description}</p>
-
-          <ul className={s.pullRequests}>
-            {contribution.prs.map((pr) => (
-              <li key={pr.url}>
-                <Link
-                  href={pr.url}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  tabIndex={activeTabId === index ? 0 : -1}
-                >
-                  {pr.title}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
+        <Panel
+          key={index}
+          contribution={contribution}
+          index={index}
+          activeTabId={activeTabId}
+        />
       ))}
     </div>
   );
