66f481b90aae0e6d3b2bb621a8094ae585d649ba
Moamal-2000 <moamalalaapro1@gmail.com>
2026-03-29 13:46:02 +0300

    refactor(ProjectCard): simplify by replacing anchor links with IconLink in ProjectLinks component

--- src/components/Home/OtherProjectsSection/OtherProjects/ProjectCard/ProjectHeader/ProjectLinks/ProjectLinks.jsx
+++ src/components/Home/OtherProjectsSection/OtherProjects/ProjectCard/ProjectHeader/ProjectLinks/ProjectLinks.jsx
@@ -1,35 +1,21 @@
+import IconLink from "@/components/Shared/Buttons/IconLink/IconLink";
 import s from "./ProjectLinks.module.scss";
 
 const ProjectLinks = ({ repoUrl, liveUrl, title }) => {
   return (
     <div className={s.projectLinks}>
-      {repoUrl && (
-        <a
-          href={repoUrl}
-          title="GitHub Link"
-          target="_blank"
-          rel="noopener noreferrer"
-          aria-label={`View ${title} source code on GitHub`}
-        >
-          <svg aria-hidden="true">
-            <use href="/icons-sprite.svg#github" />
-          </svg>
-        </a>
-      )}
-
-      {liveUrl && (
-        <a
-          href={liveUrl}
-          title="External Link"
-          target="_blank"
-          rel="noopener noreferrer"
-          aria-label={`Visit ${title} live site (opens in a new tab)`}
-        >
-          <svg aria-hidden="true">
-            <use href="/icons-sprite.svg#live" />
-          </svg>
-        </a>
-      )}
+      <IconLink
+        href={repoUrl}
+        title="GitHub Link"
+        iconName="github"
+        ariaLabel={`View ${title} source code on GitHub`}
+      />
+      <IconLink
+        href={liveUrl}
+        title="External Link"
+        iconName="live"
+        ariaLabel={`Visit ${title} live site (opens in a new tab)`}
+      />
     </div>
   );
 };
