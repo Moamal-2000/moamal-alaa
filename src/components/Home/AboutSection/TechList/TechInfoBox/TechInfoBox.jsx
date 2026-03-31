3f5b1ad04acad4bbba4c85603d599ca09d9eb1f1
Moamal-2000 <moamalalaapro1@gmail.com>
2026-03-31 09:44:07 +0300

    perf(TechInfoBox): reduce DOM nodes and enhance accessibility attributes

--- src/components/Home/AboutSection/TechList/TechInfoBox/TechInfoBox.jsx
+++ src/components/Home/AboutSection/TechList/TechInfoBox/TechInfoBox.jsx
@@ -1,20 +1,22 @@
-import { TECHNOLOGIES } from "@/data/staticData";
 import { AnimatePresence, motion } from "motion/react";
 import s from "./TechInfoBox.module.scss";
 
-const TechInfoBox = ({ activeIndex }) => {
+const TechInfoBox = ({ activeTech, id }) => {
   return (
     <AnimatePresence mode="wait">
-      <motion.div
-        key={activeIndex}
+      <motion.p
+        key={activeTech.id}
         className={s.infoBox}
         initial={{ opacity: 0, y: 5 }}
         animate={{ opacity: 1, y: 0 }}
         exit={{ opacity: 0, y: -5 }}
         transition={{ duration: 0.2, ease: "easeInOut" }}
+        id={id}
+        role="tabpanel"
+        aria-labelledby={`tab-${activeTech.id}`}
       >
-        <p>{TECHNOLOGIES[activeIndex].description}</p>
-      </motion.div>
+        {activeTech.description}
+      </motion.p>
     </AnimatePresence>
   );
 };
