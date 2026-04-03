41ec4b21df10d5fb7b9cff535feffc33c25ee147
Moamal-2000 <moamalalaapro1@gmail.com>
2026-04-03 10:58:22 +0300

    update(OtherProjects): remove DemoUnavailableOverlay after update all broken URLs

--- src/components/Home/OtherProjectsSection/OtherProjects/OtherProjects.jsx
+++ src/components/Home/OtherProjectsSection/OtherProjects/OtherProjects.jsx
@@ -1,13 +1,10 @@
 import { OTHER_PROJECTS_DATA } from "@/data/otherProjects";
-import DemoUnavailableOverlay from "../../../Shared/Alerts/DemoUnavailableOverlay/DemoUnavailableOverlay";
 import s from "./OtherProjects.module.scss";
 import ProjectCard from "./ProjectCard/ProjectCard";
 
 const OtherProjects = () => {
   return (
     <div className={s.otherProjects}>
-      <DemoUnavailableOverlay message="Access to the website demos is currently unavailable." />
-
       {OTHER_PROJECTS_DATA.map((projectData, index) => (
         <ProjectCard key={projectData.id} data={projectData} index={index} />
       ))}
