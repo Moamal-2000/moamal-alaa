abcf6ea64a744ab801adfa7094a229ef5ccca129
Moamal-2000 <moamalalaapro1@gmail.com>
2026-04-17 06:15:35 +0300

    refactor(HeaderNavLinks): remove manual onClick navigation handling

--- src/components/Header/HeaderNavLinks/HeaderNavLinks.jsx
+++ src/components/Header/HeaderNavLinks/HeaderNavLinks.jsx
@@ -3,24 +3,15 @@
 import { NAV_LINKS } from "@/data/staticData";
 import { navLinkMotionProps } from "@/lib/motionConfig";
 import { motion } from "motion/react";
-import { usePathname, useRouter } from "next/navigation";
 import s from "./HeaderNavLinks.module.scss";
 
 const HeaderNavLinks = ({ navClicked }) => {
-  const pathname = usePathname();
-  const router = useRouter();
-
-  function handleClick(title) {
-    if (pathname !== "/") router.push(`/#${title}`);
-    navClicked.current = true;
-  }
-
   return (
     <ol className={s.navLinks}>
       {NAV_LINKS.map(({ title, id }, index) => (
         <motion.li key={id} {...navLinkMotionProps(index)}>
           <a
-            onClick={() => handleClick(title.toLowerCase())}
+            onClick={() => (navClicked.current = true)}
             href={`/#${title.toLowerCase()}`}
           >
             {title}
