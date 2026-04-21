2fb4561dab1a33b83a35fd83f93d20ace8a881d3
Moamal-2000 <moamalalaapro1@gmail.com>
2026-04-21 20:38:41 +0300

    perf(next.config): prioritize AVIF with WebP fallback for unsupported browsers

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@ const nextConfig = {
   devIndicators: false,
   reactCompiler: true,
   productionBrowserSourceMaps: true,
-  images: { qualities: [100] },
+  images: { qualities: [100], formats: ["image/avif", "image/webp"] },
 };
 
 export default nextConfig;
