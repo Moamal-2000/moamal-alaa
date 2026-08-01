f80c0400e2d45ff0ca2ac3fe08f44281ab2fe527
Moamal-2000 <moamalalaapro1@gmail.com>
2026-08-01 11:15:39 +0300

    perf(head): add DNS prefetch fallback for GitHub API and canonical link

--- src/components/Shared/RootProviders.jsx
+++ src/components/Shared/RootProviders.jsx
@@ -9,7 +9,10 @@ const RootProviders = ({ children }) => {
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(personJsonLd) }}
         />
+
         <link rel="preconnect" href="https://api.github.com" />
+        <link rel="dns-prefetch" href="https://api.github.com" />
+        <link rel="canonical" href="https://www.moamalalaa.com" />
       </head>
       <Body>{children}</Body>
     </html>
