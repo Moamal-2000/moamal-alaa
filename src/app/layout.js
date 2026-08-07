61946f35e2bae42b4c1270eee4c90810d5827850
Moamal-2000 <moamalalaapro1@gmail.com>
2026-08-07 09:09:26 +0300

    refactor(layout): remove RootProviders and move document structure to RootLayout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,9 @@
+import Body from "@/components/Shared/Body";
 import { IS_PRODUCTION } from "@/data/constants";
+import { personJsonLd } from "@/data/jsonLd";
 import { METADATA } from "@/data/metadata";
 import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import RootProviders from "../components/Shared/RootProviders";
 import "../styles/globals.scss";
 
 export const metadata = METADATA;
@@ -16,7 +17,20 @@ export const viewport = {
 export default function RootLayout({ children }) {
   return (
     <>
-      <RootProviders>{children}</RootProviders>
+      <html lang="en" data-scroll-behavior="smooth">
+        <head>
+          <script
+            type="application/ld+json"
+            dangerouslySetInnerHTML={{ __html: JSON.stringify(personJsonLd) }}
+          />
+
+          <link rel="preconnect" href="https://api.github.com" />
+          <link rel="dns-prefetch" href="https://api.github.com" />
+          <link rel="canonical" href="https://www.moamalalaa.com" />
+        </head>
+        <Body>{children}</Body>
+      </html>
+
       {IS_PRODUCTION && (
         <>
           <Analytics />
