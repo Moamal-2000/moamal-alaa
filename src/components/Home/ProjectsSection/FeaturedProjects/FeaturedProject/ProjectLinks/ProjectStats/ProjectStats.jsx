c5bb386ed2229db075d27b75a8685f76b6519038
Moamal-2000 <moamalalaapro1@gmail.com>
2026-08-12 21:21:27 +0300

    chore(ProjectStats): add descriptive class names and fix missing colors and spacing

--- src/components/Home/ProjectsSection/FeaturedProjects/FeaturedProject/ProjectLinks/ProjectStats/ProjectStats.jsx
+++ src/components/Home/ProjectsSection/FeaturedProjects/FeaturedProject/ProjectLinks/ProjectStats/ProjectStats.jsx
@@ -8,16 +8,20 @@ const ProjectStats = ({ stars = 0, forks = 0, commitCount = 0 }) => {
     <dl className={s.projectStats}>
       {stats.map(({ value, icon, label }, index) => (
         <Fragment key={icon}>
-          <dt className={s.stat}>
+          <dt className={s.term}>
             <svg aria-hidden="true">
               <use href={`/icons-sprite.svg#${icon}`} />
             </svg>
             <span className="visuallyHidden">{label}</span>
           </dt>
 
-          <dd>{value.toLocaleString()}</dd>
+          <dd className={s.value}>{value.toLocaleString()}</dd>
 
-          {index < stats.length - 1 && <span aria-hidden="true">.</span>}
+          {index < stats.length - 1 && (
+            <span className={s.dot} aria-hidden="true">
+              .
+            </span>
+          )}
         </Fragment>
       ))}
     </dl>
