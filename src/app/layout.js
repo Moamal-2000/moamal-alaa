22084084adf898a0c117c0142782bdccbc0b0bd4
Moamal-2000 <moamalalaapro1@gmail.com>
2026-08-30 11:28:17 +0300

    refactor(fonts): use next/font/local instead of manual font preloading to improve LCP

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import Body from "@/components/Shared/Body";
 import { IS_PRODUCTION } from "@/constants/constants";
 import { PERSON_JSON_LD } from "@/constants/jsonLd";
 import { METADATA } from "@/constants/metadata";
+import { calibreFont, sfMonoFont } from "@/lib/fonts";
 import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { LazyMotion, domAnimation } from "motion/react";
@@ -19,22 +20,12 @@ export default function RootLayout({ children }) {
   return (
     <>
       <LazyMotion features={domAnimation} strict>
-        <html lang="en" data-scroll-behavior="smooth">
+        <html
+          lang="en"
+          className={`${calibreFont.variable} ${sfMonoFont.variable}`}
+          data-scroll-behavior="smooth"
+        >
           <head>
-            <link
-              rel="preload"
-              href="/fonts/Calibre/Calibre-Regular.woff2"
-              as="font"
-              type="font/woff2"
-              crossOrigin="anonymous"
-            />
-            <link
-              rel="preload"
-              href="/fonts/Calibre/Calibre-Semibold.woff2"
-              as="font"
-              type="font/woff2"
-              crossOrigin="anonymous"
-            />
             <script
               type="application/ld+json"
               dangerouslySetInnerHTML={{
---
+++ src/lib/fonts.js
@@ -0,0 +1,45 @@
+import localFont from "next/font/local";
+
+export const calibreFont = localFont({
+  src: [
+    {
+      path: "../../public/fonts/Calibre/Calibre-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Calibre/Calibre-Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Calibre/Calibre-Semibold.woff2",
+      weight: "600",
+      style: "normal",
+    },
+  ],
+  variable: "--font-calibre",
+  display: "swap",
+});
+
+export const sfMonoFont = localFont({
+  src: [
+    {
+      path: "../../public/fonts/SFMono/SFMono-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/SFMono/SFMono-Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/SFMono/SFMono-Semibold.woff2",
+      weight: "600",
+      style: "normal",
+    },
+  ],
+  variable: "--font-sf-mono",
+  display: "swap",
+});
