24c01e91faa2f8467befebcb0c5de6c28dd0ceb5
Moamal-2000 <moamalalaapro1@gmail.com>
2026-08-30 11:56:17 +0300

    perf(next): enable optimizeCss and install critters for critical CSS inline

--- next.config.mjs
+++ next.config.mjs
@@ -4,10 +4,15 @@ const nextConfig = {
   devIndicators: false,
   reactCompiler: true,
   productionBrowserSourceMaps: true,
+
   images: {
     qualities: [100, 75, 50],
     formats: ["image/avif", "image/webp"],
     unoptimized: false,
+  },
+
+  experimental: {
+    optimizeCss: true,
   },
 
   async redirects() {
