abe9614d792347313a6bfdf0f4b19e7dac4b45b5
Moamal-2000 <moamalalaapro1@gmail.com>
2026-08-31 07:25:04 +0300

    chore(next.config): disable redirects from legacy domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,3 @@
-import { getOldDomainRedirects } from "./src/lib/getRedirects.mjs";
-
 const nextConfig = {
   devIndicators: false,
   reactCompiler: true,
@@ -9,10 +7,6 @@ const nextConfig = {
     qualities: [100, 75, 50],
     formats: ["image/avif", "image/webp"],
     unoptimized: false,
-  },
-
-  async redirects() {
-    return getOldDomainRedirects();
   },
 };
 
