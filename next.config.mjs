a40de4dfca56b09227f2b5fccabc46ea4794780a
Moamal-2000 <moamalalaapro1@gmail.com>
2026-09-20 05:01:22 +0300

    security(headers): enable strict transport security HSTS

--- next.config.mjs
+++ next.config.mjs
@@ -19,6 +19,20 @@ const nextConfig = {
       },
     ];
   },
+
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=31536000; includeSubDomains; preload",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
