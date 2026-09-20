d73333892a512653d7ceb86fb83b09a40eae09bf
Moamal-2000 <moamalalaapro1@gmail.com>
2026-09-20 05:04:48 +0300

    security(headers): add x-frame-options and frame-ancestors to prevent clickjacking

--- next.config.mjs
+++ next.config.mjs
@@ -26,6 +26,14 @@ const nextConfig = {
         source: "/:path*",
         headers: [
           {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors 'none';",
+          },
+          {
             key: "Strict-Transport-Security",
             value: "max-age=31536000; includeSubDomains; preload",
           },
